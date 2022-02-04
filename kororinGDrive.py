adc105ee23eba6e38b23652e8d0a257ee9266cf5
Oracle Public Cloud User <opc@ringandkey-001.subnet12221411.vcn12221411.oraclevcn.com>
2022-02-04 05:56:31 +0000

    utf8でのコーディング指定と、シート内のタブソート対応

--- kororinGDrive.py
+++ kororinGDrive.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
--- kororinGSheet.py
+++ kororinGSheet.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from googleapiclient.discovery import build
 
 # 認証情報からGoogleDriveアクセスのためのサービス生成
@@ -22,3 +24,34 @@ def Update(service, spreadsheet_id, sheetname, values):
 
     return service.spreadsheets().values().batchUpdate(
         spreadsheetId=spreadsheet_id, body=body).execute()
+
+def SortTabs(service, spreadsheet_id, is_asc=True):
+    response = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    sheets   = response["sheets"]
+    names    = []
+    for s in sheets:
+        names.append(s["properties"]["title"])
+
+    if is_asc:
+        names.sort(reverse=False)
+    else:
+        names.sort(reverse=True)
+
+    requests = []
+    for s in sheets:
+        idx = names.index(s["properties"]["title"])
+        s["properties"]["index"] = idx
+        requests.append({
+            "updateSheetProperties": {
+                "properties": s["properties"],
+                "fields": "*"
+            }
+        })
+
+    request_body = {"requests": requests} 
+    return service.spreadsheets().batchUpdate(
+        spreadsheetId = spreadsheet_id,
+        body = request_body 
+    ).execute()
+
+
